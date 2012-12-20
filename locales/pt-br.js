fe2c2bb32cab097db38a95fcc6f18f129d7ec514
Alexandre Rosenfeld <airmind@gmail.com>
2012-12-20 05:49:25 -0200

    / ‘locales/pt-BR.js’ / ‘locales/pt-br.js’

--- locales/pt-br.js
+++ locales/pt-br.js
@@ -1,5 +1,5 @@
 {
-	"AIESEC Registration System": "AIESEC Registration System",
+	"AIESEC Registration System": "AIESEC Registration System ",
 	"Offices": "Escritórios Locais",
 	"National Products": "Produtos Nacionais",
 	"Registrations": "Cadastros",
